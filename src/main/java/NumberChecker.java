955195d77a120a93f0269bab29c14bc19b30c0ef
Nambi005 <nambiar.sneha123@gmail.com>
2026-03-23 10:38:28 +1100

    Create NumberChecker.java

---
+++ src/main/java/NumberChecker.java
@@ -0,0 +1,10 @@
+public class NumberChecker {
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+}
